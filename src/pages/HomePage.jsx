4b4ce0d900dd61027bac96dc5913cb4a1c366210
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-09-04 11:23:39 +0200

    Added in Header and Footer comps in layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 const HomePage = () => {
   return (
-    <div className='align-element py-20'>
-      <nav>navbar</nav>
-      <Outlet /> <Link to='/CartItem'>CartItem</Link>
-      <footer>footer</footer>
+    <div className="align-element ">
+      <Header />
+      <Outlet /> <Link to="/CartItem">CartItem</Link>
+      <Footer />{" "}
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
