e1351c10859b97d0cba1d6adb76179e7c0750e8d
EmelieJohanna <emelie.lundberg42@gmail.com>
2024-09-04 14:05:21 +0200

    initial cart things

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const CartItem = () => {
-  return <div>CartItem</div>
+function CartItem({ product }) {
+  if (!product) {
+    return <div>Cart is empty!!</div>;
+  }
+  return (
+    <div className="flex items-center space-x-4 p-4 bg-white shadow-md rounded-lg overflow-hidden border border-gray-300">
+      <img
+        src={product.img}
+        alt={product.name}
+        className="h-16 w-16 flex-none rounded-full object-cover"
+      />
+
+      <div className="flex-grow">
+        <h5 className="text-lg font-semibold">{product.name}</h5>
+        <p className="text-gray-500">${product.price.toFixed(2)}</p>
+      </div>
+
+      <input
+        type="number"
+        className="w-20 p-1 border rounded border-gray-300 text-center"
+        defaultValue={1}
+        min={1}
+      />
+
+      <div className="text-lg font-semibold">
+        Total: ${product.price.toFixed(2)}
+      </div>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import CartItem from "../components/CartItem";
 
 const CartPage = () => {
   return (
     <div>
-      {" "}
-      <Link to="/">home button</Link>
-      <Link to="/ProductPage">home button</Link>
+      <h2>Your Cart:</h2>
+      <div>CartItems</div>
+      {/* Skapat för testning, ej klar */}
+      <CartItem />
+      <div className="h-16 w-60">Cart Total: </div>
+      <button className="h-16 w-60 bg-slate-300 shadow-lg shadow-black">
+        Click to checkout
+      </button>
     </div>
   );
 };
