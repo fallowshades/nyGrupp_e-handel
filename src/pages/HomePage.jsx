a2d9870bb036392eaed88a609b5c0043002fafce
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2024-09-04 13:32:42 +0200

    Navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ProductService from "../services/ProductService";
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
   return (
     <div>
+      <Navbar />
       <ProductService />
       <Outlet /> <Link to="/CartPage">CartPage</Link>
       <footer>footer</footer>
