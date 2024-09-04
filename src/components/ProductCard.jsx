c24c5cb7aab73322f4c8f7898b4cff5407f018c8
AdamFagerberg <Adam.fagerberg1@gmail.com>
2024-09-04 11:07:26 +0200

    ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>
-}
+const ProductCard = ({ imgSrc, imgAlt, title, price, onClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={imgSrc} alt={imgAlt} />
+      </div>
+      <div>
+        <h1>{title}</h1>
+        <p>{price}</p>
+        <button onClick={onClick}>Add to cart</button>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
